a34f88b0390909961eb0a88891b0dcd8d435ada8
Héctor Iglesias <hgi834@alumnos.unican.es>
2024-03-07 14:00:13 +0100

    Implemented token storage in frontend
    -> User token is stored locally (no need to repeat the signin if the app is closed)
    -> Redirection to / rather than /home
    -> Fixed error message showing up when API is not running

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Provider store={store}>
         <Router>
           <Routes>
-            <Route path='/login' element={<LoginPage />}/>
+            <Route path='/login' element={<LoginPage />} />
           </Routes>
         </Router>
       </Provider>
--- src/main/frontend/src/state/authSlice.js
+++ src/main/frontend/src/state/authSlice.js
@@ -33,7 +33,7 @@ export const authSlice = createSlice({
     extraReducers: (builder) => {
         // login
         builder
-            .addCase(login.pending, (state, action) => {
+            .addCase(login.pending, (state) => {
                 state.currentUser = undefined;
                 state.otpSessionChallenge = undefined;
                 state.tcuToken = undefined;
@@ -47,12 +47,14 @@ export const authSlice = createSlice({
                 state.tcuToken = signInData.tcuToken;
                 state.errors = signInData.errors;
 
+                localStorage.setItem('access-token', state.currentUser.jwtToken.token);
+
                 state.loading = false;
-            }).addCase(login.rejected, (state, action) => {
+            }).addCase(login.rejected, (state) => {
                 state.otpSessionChallenge = undefined;
                 state.tcuToken = undefined;
                 state.currentUser = undefined;
-                state.errors = action.error;
+                state.errors = [];
 
                 state.loading = false;
             });
