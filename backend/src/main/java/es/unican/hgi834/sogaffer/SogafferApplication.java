cc42d66a3187596860c8ff9acdebe0d59edcbaf6
Héctor Iglesias <hgi834@alumnos.unican.es>
2026-05-03 16:25:15 +0200

    feat: initialize backend project

---
+++ backend/src/main/java/es/unican/hgi834/sogaffer/SogafferApplication.java
@@ -0,0 +1,13 @@
+package es.unican.hgi834.sogaffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogafferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SogafferApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/es/unican/hgi834/sogaffer/SogafferApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.hgi834.sogaffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogafferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
