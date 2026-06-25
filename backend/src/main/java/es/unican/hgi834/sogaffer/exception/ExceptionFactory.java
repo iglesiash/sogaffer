0b37305db69dffea374cd7732fe89539ec9ef8b4
Héctor Iglesias <hgi834@alumnos.unican.es>
2026-06-25 19:52:48 +0200

    feat: add handlers for Bad Request
    - Add `badRequest` method in ExceptionFactory
    - Add Exception classes which should call this method when handled
    - Add InvalidCredentialsException to call `unauthorized` when handled

--- backend/src/main/java/es/unican/hgi834/sogaffer/exception/ExceptionFactory.java
+++ backend/src/main/java/es/unican/hgi834/sogaffer/exception/ExceptionFactory.java
@@ -3,6 +3,11 @@ package es.unican.hgi834.sogaffer.exception;
 import org.springframework.http.HttpStatus;
 
 public class ExceptionFactory {
+
+    public static SogafferErrorDto badRequest(String message) {
+        return new SogafferErrorDto(HttpStatus.BAD_REQUEST.value(), "Bad Request", message);
+    }
+
     public static SogafferErrorDto internalServerError(String message) {
         return new SogafferErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.value(),"Internal Server Error", message);
     }
--- backend/src/main/java/es/unican/hgi834/sogaffer/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/es/unican/hgi834/sogaffer/exception/GlobalExceptionHandler.java
@@ -2,14 +2,27 @@ package es.unican.hgi834.sogaffer.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(SorareAuthException.class)
-    public ResponseEntity<SogafferErrorDto> handleInvalidLogin(SorareAuthException e) {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<SogafferErrorDto> handleBadRequest(Exception e) {
+        SogafferErrorDto error = ExceptionFactory.badRequest(e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
+
+    @ExceptionHandler({SorareAuthException.class, InvalidCredentialsException.class})
+    public ResponseEntity<SogafferErrorDto> handleInvalidLogin(Exception e) {
         SogafferErrorDto error = ExceptionFactory.unauthorized(e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(error);
@@ -17,7 +30,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<SogafferErrorDto> handleAnyOtherException(Exception e) {
-
         SogafferErrorDto error = ExceptionFactory.internalServerError(e.getMessage());
 
         return ResponseEntity
